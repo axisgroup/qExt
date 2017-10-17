40f4d1918ef0466f3129512d53f5bed4c6488d92
John Bellizzi <jabellizzi406@gmail.com>
2017-10-17 15:04:04 -0400

    adding input

--- bin/demo-global.js
+++ bin/demo-global.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
+// Delete 0 and 1 argument
+var args = process.argv.splice(process.execArgv.length + 2);
+
+// Retrieve first argument
+var name = args[0];
+
 var myLibrary = require('../lib/index.js');
 
 // Displays the text in the console
-myLibrary.say('library');
+myLibrary.say('Hello ' +name);
