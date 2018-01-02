11adb5d49a6a047bf5b03523378f409377b55773
John Bellizzi <jabellizzi406@gmail.com>
2018-01-02 09:04:26 -0500

    added install flag

--- bin/qext.js
+++ bin/qext.js
@@ -6,14 +6,18 @@ var path = require('path');
 var program = require('commander');
 var childProcess = require('child_process');
 
-var CreateExtension = require(path.resolve(__dirname, '../scripts/create-Extension.js'));
+var CreateExtension = require(path.resolve(__dirname, '../scripts/create-extension.js'));
+var InstallDependencies = require(path.resolve(__dirname, '../scripts/install-dependencies.js'));
 
 program
   .version('0.1.4')
   .option('-c, --create-extension [name]', 'Create New Extension')
+  .option('-i, --install', 'Install Dependencies')
   .parse(process.argv);
 
 
 var extensionName = typeof(program.createExtension) === 'boolean' ? 'new-extension' : program.createExtension;
 
 if(program.createExtension) CreateExtension(extensionName);
+
+if(program.install) InstallDependencies(extensionName);
---
+++ scripts/install-dependencies.js
@@ -0,0 +1,10 @@
+module.exports = function(extensionName) {
+  var childProcess = require('child_process');
+
+  // install qext-scripts
+  childProcess.spawn(
+    'npm',
+    ['install', '--save', 'qext-scripts'],
+    { env: process.env, cwd: process.cwd() +`/${extensionName}`, stdio: 'inherit' }
+  )
+}
