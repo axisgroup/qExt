4ee4982be668a7cd2f700cbdb6d2ddef1e0b3984
John Bellizzi <jabellizzi406@gmail.com>
2018-02-07 15:58:49 -0500

    changed template name to extension-template

--- scripts/create-extension.js
+++ scripts/create-extension.js
@@ -53,11 +53,11 @@ module.exports = function(extensionName) {
   =============================== */
   if(!fs.pathExistsSync(path.resolve(`${process.cwd()}/${extensionName}/src`))) {
     // Copy Template extension files into new project
-    fs.copySync(path.resolve(__dirname, '../new-extension'), `./${extensionName}/src`);
+    fs.copySync(path.resolve(__dirname, '../extension-template'), `./${extensionName}/src`);
 
     // Rename main js and qext files to extension name
-    fs.moveSync(path.resolve(`${process.cwd()}/${extensionName}/src/new-extension.js`), `${process.cwd()}/${extensionName}/src/${extensionName}.js`);
-    fs.moveSync(path.resolve(`${process.cwd()}/${extensionName}/src/new-extension.qext`), `${process.cwd()}/${extensionName}/src/${extensionName}.qext`);
+    fs.moveSync(path.resolve(`${process.cwd()}/${extensionName}/src/extension-template.js`), `${process.cwd()}/${extensionName}/src/${extensionName}.js`);
+    fs.moveSync(path.resolve(`${process.cwd()}/${extensionName}/src/extension-template.qext`), `${process.cwd()}/${extensionName}/src/${extensionName}.qext`);
 
     fs.copySync(path.resolve(__dirname, '../config/server.config.json'), `./${extensionName}/server.config.json`);
   } else {
