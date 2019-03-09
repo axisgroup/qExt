c8f984254c6a9bcc113432a193c90f4968d5af98
John Bellizzi <jabellizzi406@gmail.com>
2019-03-08 23:50:24 -0500

    added vanilla-entry property check test

--- packages/qext-scripts/src/components/qextConfig.js
+++ packages/qext-scripts/src/components/qextConfig.js
@@ -21,31 +21,40 @@ export default inputAccessorFunction => {
 
 					configFileExists
 						.then(config => {
-							/** extension property not defined */
+							// extension property not defined
 							if (config.extension === undefined)
 								observer.error(
 									`extension property not defined in ${configFile}`
 								)
-							/** output property not defined */ else if (
-								config.output === undefined
-							)
+							// output property not defined
+							else if (config.output === undefined)
 								observer.error(`output property not defined in ${configFile}`)
-							/** mode property not defined */ else if (
-								config.mode === undefined
-							)
+							// mode property not defined
+							else if (config.mode === undefined)
 								observer.error(`mode property not defined in ${configFile}`)
-							/** mode value not vanilla or compile */ else if (
-								["vanilla", "compile"].indexOf(config.mode) === -1
-							)
+							// mode value not vanilla or compile
+							else if (["vanilla", "compile"].indexOf(config.mode) === -1)
 								observer.error(
 									`mode value must be set to "vanilla" or "compile"`
 								)
-							/** vanilla property not defined when mode: vanilla */ else if (
-								config.mode === "vanilla" &&
-								config.vanilla === undefined
-							)
-								observer.error(`vanilla property not defined in ${configFile}`)
-							/** pass */ else {
+							// Vanilla mode
+							else if (config.mode === "vanilla") {
+								// vanilla property not defined
+								if (config.vanilla === undefined)
+									observer.error(
+										`vanilla property not defined in ${configFile}`
+									)
+								// vanilla-entry property not defined
+								else if (config.vanilla.entry === undefined)
+									observer.error(`entry property not defined in "vanilla"`)
+								// pass
+								else {
+									observer.next(config)
+									observer.complete()
+								}
+							}
+							// pass
+							else {
 								observer.next(config)
 								observer.complete()
 							}
