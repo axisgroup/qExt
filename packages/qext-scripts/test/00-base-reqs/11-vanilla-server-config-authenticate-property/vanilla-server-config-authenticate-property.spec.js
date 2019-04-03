058d16be7e73ef0fa88af97d02e26d76454a3936
John Bellizzi <jabellizzi406@gmail.com>
2019-04-03 11:12:41 -0400

    authenticate property

--- packages/qext-scripts/test/00-base-reqs/11-vanilla-server-config-authenticate-property/vanilla-server-config-authenticate-property.spec.js
+++ packages/qext-scripts/test/00-base-reqs/11-vanilla-server-config-authenticate-property/vanilla-server-config-authenticate-property.spec.js
@@ -6,13 +6,13 @@ const execQextScripts = require("../../util/exec-qext-scripts")
 const testExtensionDir = path.resolve(__dirname, "./TestExtension")
 const extensionName = "test-extension"
 
-describe("10. Server Config property", function() {
+describe("11. Server Config authenticate property", function() {
 	this.timeout(10000)
 
-	it("should fail if serverConfig is not defined", done => {
+	it("should fail if authenticate is not defined", done => {
 		execQextScripts(testExtensionDir, (error, stdout, stderr) => {
 			stderr.should.equal(
-				`serverConfig property not defined in ./qext.config.json\n`
+				`authenticate property not defined in ./qext.config.json\n`
 			)
 			done()
 		})
