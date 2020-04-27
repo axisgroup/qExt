812ce85f570ce387e29a9bfd4ea14a3ba2eaf84c
John Bellizzi <jbellizzi@axisgroup.com>
2020-04-27 13:50:29 -0400

    adding html and style loaders

--- packages/qext-scripts/example/src/index.js
+++ packages/qext-scripts/example/src/index.js
@@ -1,4 +1,11 @@
+import localCSS from "./style.css"
+
 window.define([], function() {
+	const head = document.querySelector("head")
+	const style = document.createElement("style")
+	style.innerHTML = localCSS.toString()
+	head.appendChild(style)
+
 	return {
 		paint: () => console.log("paint"),
 	}
---
+++ packages/qext-scripts/test/build/compile-css/src/index.js
@@ -0,0 +1,5 @@
+import css from "./style.css"
+
+export default window.define([], function() {
+	return {}
+})
