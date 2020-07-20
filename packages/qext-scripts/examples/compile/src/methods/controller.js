28baa1eeb7786bab10570b92eeaa9521efa24d11
John Bellizzi <jbellizzi@axisgroup.com>
2020-07-20 17:56:46 -0400

    removing @babel/plugin-transform-modules-commonjs from webpack

---
+++ packages/qext-scripts/examples/compile/src/methods/controller.js
@@ -0,0 +1,17 @@
+import { Subject } from "rxjs"
+import { map } from "rxjs/operators"
+// import { select } from "d3-selection"
+
+export default [
+	"$scope",
+	"$element",
+	function($scope, $element) {
+		// const layout$ = new Subject()
+		// const resize$ = new Subject()
+		// $scope.viz = { layout$, resize$ }
+		// const plot = select($element[0])
+		// 	.select(".chart-container")
+		// 	.select("svg")
+		// 	.select("g.plot")
+	},
+]
