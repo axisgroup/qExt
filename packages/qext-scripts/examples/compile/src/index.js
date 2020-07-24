32beb333d00d3ac24f42d1695597b28fd49642b2
John Bellizzi <jbellizzi@axisgroup.com>
2020-07-24 12:28:32 -0400

    cleaning up examples

--- packages/qext-scripts/examples/compile/src/index.js
+++ packages/qext-scripts/examples/compile/src/index.js
@@ -6,33 +6,15 @@
 // 	paint,
 // 	resize,
 // } from "./methods"
-import { Subject } from "rxjs/_esm2015"
-import { map } from "rxjs/_esm2015/operators"
 import "./style.css"
 
 window.define([], function() {
-	const element = document.createElement("pre")
-
-	const a$ = new Subject()
-
-	a$.pipe(
-		map(a => a)
-		// tap(a => a)
-	).subscribe(console.log)
-
-	a$.next("webpack")
-	// Lodash, currently included via a script, is required for this line to work
-	// element.innerHTML = join(["Hello", "webpack"], " ")
-	element.innerHTML = ["Hello webpack", "5 cubed is equal to " + 125].join("\n\n")
-	element.classList.add("hello")
-
-	return element
-	// return {
-	// 	initialProperties,
-	// 	template,
-	// 	definition,
-	// 	controller,
-	// 	paint,
-	// 	resize,
-	// }
+	return {
+		// initialProperties,
+		// template,
+		// definition,
+		// controller,
+		// paint,
+		// resize,
+	}
 })
